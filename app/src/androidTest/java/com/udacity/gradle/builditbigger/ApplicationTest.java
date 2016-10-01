a6d04b93c90aeb2d5a02b6ed73adddd25a1c4047
Akhilesh Kumar <akhilesh@codemymobile.com>
2016-10-01 14:25:38 +0530

    testing asyncTask added

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
@@ -2,12 +2,35 @@ package com.udacity.gradle.builditbigger;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.util.concurrent.CountDownLatch;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+    private String mJoke;
+
     public ApplicationTest() {
         super(Application.class);
+    }
+
+
+    // fetch joke AsyncTask test
+    public void testFetchJokeAsync() throws InterruptedException {
+        final CountDownLatch latch = new CountDownLatch(1);
+        new JokeFetchAsyncTask(new JokeFetchListener() {
+            @Override
+            public void onJokeFetch(String joke) {
+                mJoke = joke;
+                latch.countDown();
+            }
+        }).execute();
+
+        latch.await();
+        Log.d("TESTING", mJoke);
+        assertFalse(TextUtils.isEmpty(mJoke));
     }
 }
